67507b1cf1519a5d93eede389cf8989f77ad0a57
Sawan Gurjar <49248653+SawanGurjar@users.noreply.github.com>
2021-11-21 01:20:09 +0530

    Delete gurobi_setup_path.sh

--- gurobi_setup_path.sh
+++
@@ -1,4 +0,0 @@
-export GUROBI_HOME="$(pwd)/gurobi903/linux64"
-export PATH="${PATH}:${GUROBI_HOME}/bin"
-export CPATH="${CPATH}:${GUROBI_HOME}/include"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${GUROBI_HOME}/lib
